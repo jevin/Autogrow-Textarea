7ea3d3e66b76a2027ed2ac760af7b66085695511
dorian-marchal <marchal.dorian@gmail.com>
2015-01-28 00:45:41 +0100

    Encode whitespaces before sending content to the mirror
    Fix a bug where multiple spaces in textarea caused an offset

--- jquery.autogrowtextarea.js
+++ jquery.autogrowtextarea.js
@@ -24,7 +24,16 @@ jQuery.fn.autoGrow = function() {
 		}
 
 		var sendContentToMirror = function (textarea) {
-			mirror.innerHTML = String(textarea.value).replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/'/g, '&#39;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '<br />') + '.<br/>.';
+			mirror.innerHTML = String(textarea.value)
+				.replace(/&/g, '&amp;')
+				.replace(/"/g, '&quot;')
+				.replace(/'/g, '&#39;')
+				.replace(/</g, '&lt;')
+				.replace(/>/g, '&gt;')
+				.replace(/ /g, '&nbsp;')
+				.replace(/\n/g, '<br />') +
+				'.<br/>.'
+			;
 
 			if (jQuery(textarea).height() != jQuery(mirror).height())
 				jQuery(textarea).height(jQuery(mirror).height());
